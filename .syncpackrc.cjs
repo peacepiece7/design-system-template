8e76511c1e16f8d4c44550e993228735c6aee9d6
peacepiece7 <peacepiece7@naver.com>
2024-08-02 18:29:23 +0900

    synpack 추가 (HighestSemVer 특정 패키지에 따라서 ^18, ^19 되는지 테스트 중)

--- .syncpackrc.cjs
+++ .syncpackrc.cjs
@@ -1,15 +1,60 @@
+// @ts-check
+
+/** @type {import("syncpack").RcFile} */
 module.exports = {
-  dependencyTypes: ['dev', 'peer', 'prod'],
+  dependencyTypes: ['dev', 'prod', 'peer'],
   semverRange: '^',
   source: ['package.json', 'apps/*/package.json', 'packages/*/package.json'],
   versionGroups: [
-    {
-      label:
-        'Internal config packages should be pinned to "*" (meaning any version is acceptable)',
-      packages: ['**'],
-      dependencies: ['config-prettier', 'config-tsconfig', 'eslint-config-tds'],
-      dependencyTypes: ['dev'],
-      pinVersion: '*',
-    },
+    // {
+    //   label:
+    //     'Internal config packages should be pinned to "*" (meaning any version is acceptable)',
+    //   packages: ['**'],
+    //   dependencies: [
+    //     'config-eslint',
+    //     'config-tailwind',
+    //     'confing-ts',
+    //     'ui-shadcn',
+    //   ],
+    //   dependencyTypes: ['dev', 'prod', 'peer'],
+    //   pinVersion: '*',
+    // },
+    // {
+    //   semverRange: '^',
+    //   label: 'use react 19 in apps/web and apps/docs',
+    //   packages: ['@app/web', '@app/docs'],
+    //   dependencies: ['react', 'react-dom', 'next'],
+    //   isIgnored: true,
+    // },
+    // {
+    //   // Banned
+    //   label: 'Please use lodash instead of underscore',
+    //   packages: ['**'],
+    //   dependencies: ['underscore'],
+    //   isBanned: true,
+    // },
+    // {
+    //   // Pinned
+    //   label: "Use '*' under 'peerDependencies' everywhere",
+    //   packages: ['**'],
+    //   dependencies: ['**'],
+    //   dependencyTypes: ['peer'],
+    //   pinVersion: '*',
+    // },
   ],
+  // semverGroups: [
+  //   {
+  //     semverRange: '^',
+  //     label: 'use react 19 in apps/web and apps/docs',
+  //     packages: ['@app/web', '@app/docs'],
+  //     dependencies: ['react', 'react-dom', 'next'],
+  //     isIgnored: true,
+  //   },
+  //   {
+  //     semverRange: '^',
+  //     label: 'use react 18 in apps/storybook and other packages/**',
+  //     packages: ['@repo/**', '@app/storybook'],
+  //     dependencies: ['react', 'react-dom'],
+  //   },
+  // ],
 }
